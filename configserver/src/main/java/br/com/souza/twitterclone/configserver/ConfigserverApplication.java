211e28c8667faef477ed6b35952dc3a850b672db
Vitor Souza <vitor.souza@absoftware.com.br>
2023-07-11 11:53:12 -0300

    adicionando configserver

---
+++ configserver/src/main/java/br/com/souza/twitterclone/configserver/ConfigserverApplication.java
@@ -0,0 +1,15 @@
+package br.com.souza.twitterclone.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
